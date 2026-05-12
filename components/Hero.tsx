cc9b4753f02e35e0ce63a99d338b0aedeb552bac
garik-korolev <garik-korolev@ya.ru>
2026-05-12 13:58:12 +0300

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,23 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container hero-content">
+        <h1>
+          Иногда достаточно одного честного разговора,
+          чтобы начать менять свою жизнь
+        </h1>
+
+        <p>
+          Индивидуальные психологические консультации
+          в гештальт-подходе.
+          Поддержка в кризисах, тревоге, отношениях
+          и поиске себя.
+        </p>
+
+        <a href="/contacts" className="button">
+          Записаться на консультацию
+        </a>
+      </div>
+    </section>
+  )
+}
