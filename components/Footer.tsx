ed01aad0d6dc534869863608a105678247cc6a1b
garik-korolev <garik-korolev@ya.ru>
2026-05-12 13:57:47 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>© 2026 Психолог | Гештальт-терапия</p>
+      </div>
+    </footer>
+  )
+}
