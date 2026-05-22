85f990752adbb0cfc3510da33da269db0e0ad801
garik-korolev <garik-korolev@ya.ru>
2026-05-22 12:48:00 +0300

    Update page.tsx
    обновление контактов

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,29 +1,45 @@
 import BookingForm from '../../components/BookingForm'
+
 export default function ContactsPage() {
   return (
     <main className="container">
+
       <div className="page-title">
         <h1>Контакты</h1>
       </div>
 
       <section className="section">
+
         <div className="contact-list">
+
           <div className="card">
             <h3>Telegram</h3>
-            <p>@your_telegram</p>
+            <p>@Galina_Koroleva</p>
           </div>
 
           <div className="card">
             <h3>Email</h3>
-            <p>psychologist@example.com</p>
+            <p>nexy@bk.ru</p>
+          </div>
+
+          <div className="card">
+            <h3>Телефон</h3>
+            <p>+7 (999) 223-25-01</p>
           </div>
 
           <div className="card">
             <h3>Формат работы</h3>
-            <p>Онлайн-консультации и очные встречи</p>
+            <p>
+              Онлайн-консультации и очные встречи
+            </p>
           </div>
+
         </div>
+
+        <BookingForm />
+
       </section>
+
     </main>
   )
 }
